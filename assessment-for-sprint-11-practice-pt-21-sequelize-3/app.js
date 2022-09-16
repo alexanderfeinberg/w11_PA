4b7998cef50470ebebf71b412bb26cf31a3de199
calvintzeng96 <calvintzeng96@hotmail.com>
2022-09-16 12:39:05 -0400

    phase 3 q1q2 done

--- assessment-for-sprint-11-practice-pt-21-sequelize-3/app.js
+++ assessment-for-sprint-11-practice-pt-21-sequelize-3/app.js
@@ -1,9 +1,41 @@
 require('express-async-errors');
 require('dotenv').config();
+const {WarehouseItem, sequelize} = require('./db/models') //importing models
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.get("/items", async (req, res) => {
+
+  const items = await WarehouseItem.findAll({
+    where: {
+      isUsed: false
+    }
+  })
+
+  res.json(items)
+})
+
+app.get("/items/:name", async (req,res) => {
+
+  const item = await WarehouseItem.findOne({
+    where: {
+      name: req.params.name
+    }
+  })
+
+  if (!item) {
+    res.statusCode = 404
+    res.json({"message": "Warehouse Item not found"})
+  }
+
+  res.json(item)
+})
+
+
+
+
 
 if (require.main === module) {
   const port = 8003;
